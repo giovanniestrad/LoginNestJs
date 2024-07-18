035b8dede50b4b5ccec367440ae7e8c396aa7eae
giovanniestrad <giovanniestradabustos@gmail.com>
2024-07-17 19:40:18 -0600

    Add files via upload

---
+++ dist/cats/entities/cat.entity.d.ts
@@ -0,0 +1,8 @@
+import { Breed } from "src/breeds/entities/breed.entity";
+export declare class Cat {
+    id: number;
+    name: string;
+    age: number;
+    deletedAt: Date;
+    breed: Breed;
+}
